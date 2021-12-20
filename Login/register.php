5d91c45ba1ba74aa116b07e30fff51693f3b668e
thewaruci <92907268+thewaruci@users.noreply.github.com>
2021-12-21 06:29:56 +0700

    Update register.php

--- Login/register.php
+++ Login/register.php
@@ -4,7 +4,7 @@ error_reporting(0);
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location: /UAS/index.php");
+    header("Location: /index.php");
 }
  
 if (isset($_POST['submit'])) {
